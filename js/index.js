ad2531a82099dfdc087a992eb4b46966f0189860
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-03-03 17:09:57 +0100

    Avevi iproprio rgione ste! project

--- js/index.js
+++ js/index.js
@@ -115,8 +115,9 @@ function appendElements(parent, children) {
     parent.appendChild(child);
   });
 }
-var myCarousel = document.querySelector("#carouselExampleIndicators");
+/* var myCarousel = document.querySelector("#carouselExampleIndicators");
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: true,
 });
+ */
